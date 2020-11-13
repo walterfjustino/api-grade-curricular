61cf24bea8d352453fa804c13705343664dc8630
Walter Fernandes Justino <walterfjustino@gmail.com>
2020-11-13 20:01:15 -0300

    Created the first controller

--- src/main/java/com/rasmoo/cliente/escola/gradescurricular/GradesCurricularApplication.java
+++ src/main/java/com/rasmoo/cliente/escola/gradescurricular/GradesCurricularApplication.java
@@ -2,9 +2,16 @@ package com.rasmoo.cliente.escola.gradescurricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GradesCurricularApplication {
+public class GradesCurricularApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradesCurricularApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradesCurricularApplication.class, args);
---
+++ src/main/java/com/rasmoo/cliente/escola/gradescurricular/controller/MateriaController.java
@@ -0,0 +1,20 @@
+package com.rasmoo.cliente.escola.gradescurricular.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping("/materia")
+public class MateriaController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> helloWorldRest(){
+        return ResponseEntity.status(HttpStatus.OK).body("olá mundo REST!");
+    }
+}
